d4adf4e2ef9b50a8cc53cd5de9715aef31fe4b58
naifxdD <naifaldair@gmail.com>
2025-11-19 14:41:53 +0300

    third commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 
@@ -15,4 +14,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "/InkVision",
 }));
